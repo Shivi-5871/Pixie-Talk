c2ec6a38edb2c874e5a266530576e689e572ca52
Shivi Agawal <shiviagarwal253@gmail.com>
2025-09-23 21:54:17 +0530

    Updated layout for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import type React from "react"
-import { Sidebar } from "@/components/dashboard/sidebar"
 import { DashboardHeader } from "@/components/dashboard/header"
 
 export default function DashboardLayout({
@@ -11,7 +10,6 @@ export default function DashboardLayout({
     <div className="flex min-h-screen flex-col">
       <DashboardHeader />
       <div className="flex flex-1">
-        <Sidebar />
         <main className="flex-1 p-6 md:p-8">{children}</main>
       </div>
     </div>
